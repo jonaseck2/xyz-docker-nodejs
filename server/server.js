e7a846fa7dd6bc7d600f60edec23de7f25634674
Torbjorn Lundberg <torbjorn.lundberg@softhouse.se>
2015-03-26 18:20:13 +0100

    Just getting *something* running

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 // You probably want to change 'dist/' to '../dist/' for production
-app.use('/', express.static(path.join(__dirname, 'dist/')));
+app.use('/', express.static(path.join(__dirname, '../src/')));
 var server = app.listen(9000, function () {
     console.log('Server started: http://localhost:%s/', server.address().port);
 });
