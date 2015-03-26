71f2075d151907a4654795c0512da037eb2ef65b
Torbjorn Lundberg <torbjorn.lundberg@softhouse.se>
2015-03-26 23:41:57 +0100

    Dummy backend impl for getting user email

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
+app.get('/userEmail', function (req, res) {
+    res.send(JSON.stringify({email: 'tobbe@softhouse.se'}));
+});
+
 // You probably want to change 'dist/' to '../dist/' for production
 app.use('/', express.static(path.join(__dirname, '../src/')));
 var server = app.listen(9000, function () {
